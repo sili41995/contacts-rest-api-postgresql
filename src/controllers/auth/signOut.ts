63294a53f5618dc5b475f3581d76751144c0fabd
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-05-19 14:36:14 +0300

    update

---
+++ src/controllers/auth/signOut.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Response } from 'express';
+import { IAuthRequest, IUser } from '../../types/types';
+import { ctrlWrapper } from '../../utils';
+import prisma from 'server';
+
+const signOut = async (req: IAuthRequest, res: Response, next: NextFunction): Promise<void> => {
+  const { id } = req.user as IUser;
+  await prisma.user.update({ where: { id }, data: { token: null } });
+
+  res.status(204).json();
+};
+
+export default ctrlWrapper<IAuthRequest>(signOut);
--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-import { signUp, signIn } from '../../controllers/auth';
-import { validateBody, upload } from '../../middlewares';
+import { signUp, signIn, signOut } from '../../controllers/auth';
+import { validateBody, upload, authenticate } from '../../middlewares';
 import { userSchema } from '../../schemas';
 
 const router = express.Router();
 
 router.post('/signup', upload.single('avatar'), validateBody(userSchema.signUpSchema), signUp);
 router.post('/signin', validateBody(userSchema.signInSchema), signIn);
-// router.post('/signout', authenticate, signOut);
+router.post('/signout', authenticate, signOut);
 // router.get('/current', authenticate, current);
 // router.patch('/avatars', authenticate, upload.single('avatar'), presentFile, updateAvatar);
 
